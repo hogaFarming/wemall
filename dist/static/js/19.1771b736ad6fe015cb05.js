22ffa7ed9848d6a342e51bc52a3f38b527d9b1f9
chenhaojia <312544538@qq.com>
2017-12-14 01:20:11 +0800

    dist

---
+++ dist/static/js/19.1771b736ad6fe015cb05.js
@@ -0,0 +1,2 @@
+webpackJsonp([19],{"5/rY":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("优惠券中心")])},staticRenderFns:[]},r=n("/Xao")(null,l,!1,null,null,null);t.default=r.exports}});
+//# sourceMappingURL=19.1771b736ad6fe015cb05.js.map
