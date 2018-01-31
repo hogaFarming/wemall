825919bd349eb357791aeb7f94fadefd2bacac60
hogaFarming <312544538@qq.com>
2018-01-31 17:28:33 +0800

    首页调整

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="page home-page">
-    <x-cell class="bdb" @click.native="gotoSearch">
-      <x-icon type="search"></x-icon> 搜索
-    </x-cell>
+    <!--<x-cell class="bdb" @click.native="gotoSearch">-->
+      <!--<x-icon type="search"></x-icon> 搜索-->
+    <!--</x-cell>-->
     <!--轮播图-->
     <div class="top-slider-wrap bdb">
       <x-slider class="bdb" indicator="dots" :length="banners.length">
@@ -11,6 +11,13 @@
         </x-slider-item>
       </x-slider>
     </div>
+    <ul class="action-list">
+      <li v-for="action in actions" :key="action.url" class="action-item" @click.native="toActionUrl(action.url)">
+        <x-icon :type="action.icon"></x-icon>
+        <br>
+        <span>{{ action.label }}</span>
+      </li>
+    </ul>
     <ul class="cover-list">
       <li v-for="item in covers">
         <img @click="toLink(item.link)" :src="item.cover" alt="cover">
@@ -23,7 +30,13 @@
     data () {
       return {
         banners: [],
-        covers: []
+        covers: [],
+        actions: [
+          { icon: 'coupon01', label: '充值得积分', url: '/my/balance-recharge' },
+          { icon: 'coupon01', label: '积分兑换', url: '/product/exchange' },
+          { icon: 'coupon01', label: '福利兑换', url: 'my/cards-exchange' },
+          { icon: 'coupon01', label: '游戏', url: '/games' }
+        ]
       }
     },
     mounted () {
@@ -39,6 +52,10 @@
       gotoSearch () {
         this.$router.push('/product/search')
       },
+      toActionUrl (path) {
+        if (!path) return
+        this.$router.push(path)
+      },
       toLink (link) {
         if (!link) return
         location.href = link
@@ -47,6 +64,24 @@
   }
 </script>
 <style>
+  .top-slider-wrap {
+    position: relative;
+  }
+  .top-slider-wrap .x-slider-item img {
+    height: 5.3333rem;
+  }
+  .action-list {
+    display: flex;
+    padding: 0.48rem 0;
+  }
+  .action-item {
+    flex: 1;
+    text-align: center;
+  }
+  .action-item > .x-icon {
+    height: auto;
+    margin-bottom: 0.32rem;
+  }
   .cover-list li {
 
   }
