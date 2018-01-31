daac21ee56eeb1450706effc0dd65818aadab090
chenhaojia <312544538@qq.com>
2018-01-31 23:30:15 +0800

    修改home

--- src/views/product/category.vue
+++ src/views/product/category.vue
@@ -1,3 +1,146 @@
 <template>
-  <h1>商品分类</h1>
+  <div class="page category-page">
+    <x-cell class="bdb">
+      <x-search v-model="searchText" @search="search"></x-search>
+    </x-cell>
+    <div class="category-main">
+      <div class="category-nav-container">
+        <ul class="category-nav">
+          <li
+            class="category-nav-item"
+            v-for="cat in categories"
+            @click="selectCategory(cat.id)"
+            :key="cat.id">
+            <span>{{ cat.name }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="category-content">
+        <div class="category-sub-wrap" v-for="subCat in subCategories" :key="subCat.id" v-show="subCat.children && subCat.children.length > 0">
+          <div class="category-sub-title">{{ subCat.name }}</div>
+          <ul class="category-sub-list">
+            <li class="category-sub-item" v-for="subItem in subCat.children" :key="subItem.id" @click="onClickSubItem(subItem)">
+              <x-image class="category-sub-image" :src="subItem.logo" width="1.6rem" height="1.6rem"></x-image>
+              <div class="category-sub-label">{{ subItem.name }}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        searchText: '',
+        categories: [],
+        currentCat: null
+      }
+    },
+    computed: {
+      subCategories () {
+        const currentCatObj = this.categories.filter(item => item.id === this.currentCat)[0]
+        if (currentCatObj) {
+          return currentCatObj.children
+        }
+        return []
+      }
+    },
+    mounted () {
+      this.queryCategories()
+    },
+    methods: {
+      search () {
+        if (!this.searchText) {
+          return this.$toast('请输入您要搜索的商品')
+        }
+        this.$router.push({ path: '/product/list', query: { goods_name: this.searchText } })
+      },
+      queryCategories () {
+        return this.$http.withLoading('/api/goods/categorys')
+          .then(res => {
+            if (res && res.list) {
+              for (let i in res.list) {
+                res.list[i].next_selected = undefined
+              }
+              this.categories = res.list
+            }
+          })
+      },
+      selectCategory (id) {
+        this.currentCat = id
+      },
+      onClickSubItem (item) {
+
+      }
+    }
+  }
+</script>
+<style>
+  .category-page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  .category-main {
+    position: relative;
+    flex: 1;
+    display: flex;
+  }
+  .category-nav-container {
+    overflow: auto;
+    /*position: absolute;*/
+    /*top: 0;*/
+    /*left: 0;*/
+    /*bottom: 0;*/
+    width: 2.15rem;
+  }
+  .category-nav {
+    width: 100%;
+  }
+  .category-nav-item {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 0 0.2rem;
+    line-height: 3;
+    text-align: center;
+  }
+  .category-nav-item > span {
+
+  }
+  .category-content {
+    flex: 1;
+    background-color: #fff;
+    overflow: auto;
+    padding-top: 0.5333rem;
+  }
+  .category-sub-wrap {
+  }
+  .category-sub-wrap:after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+  .category-sub-title {
+    text-align: center;
+    margin-bottom: 0.5333rem;
+    font-size: 12px;
+  }
+  .category-sub-item {
+    width: 2.616rem;
+    float: left;
+    margin-bottom: 0.8533rem;
+  }
+  .category-sub-image {
+    width: 1.6rem;
+    height: 1.6rem;
+    margin-left: 0.51rem;
+    margin-bottom: 0.2667rem;
+  }
+  .category-sub-label {
+    font-size: 12px;
+    text-align: center;
+  }
+</style>
