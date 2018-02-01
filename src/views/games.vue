231323dc888ca0cc4cb53ab543110996fd640a94
chenhaojia <312544538@qq.com>
2018-02-02 02:11:05 +0800

    game url

--- src/views/games.vue
+++ src/views/games.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    <a href="/sega/index.html">
+    <a href="/sega.html">
       <img src="/static/img/game_sega.jpg" alt="" style="width: 100%;margin: 10px 0;">
     </a>
     <a href="/brnn/index.html">
